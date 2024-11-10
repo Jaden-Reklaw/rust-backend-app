3c8390d379a74b84cca99ee7ce19ef0966a91e7b
Jaden-Reklaw <jwalker117@gmail.com>
2024-11-09 21:40:06 -0600

    hello world with json using rocket and serde

--- src/controller/user_controller.rs
+++
@@ -1,3 +0,0 @@
-pub struct user_controller {
-    
-}
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,14 @@
-mod api;
-mod model;
-mod repository;
-mod data;
+use rocket::{self, get, routes, serde::json::Json};
 
-use api::user_api::get_user_routes;
-use repository::user_repo;
-use rocket::{launch, build};
-use data::db_client::DBClient;
-
-#[launch]
-fn rocket() -> _ {
-    let db = DBClient::init();
-    let user_repo = user_repo::UserRepo::new(&db);
-    build()
-    .manage(db)
-    .manage(user_repo)
-        .mount("/", get_user_routes())
+#[get("/")]
+fn index() -> Json<String> {
+    Json("Hello, world!".to_string())
+}
+#[rocket::main]
+async fn main() {
+    rocket::build()
+        .mount("/", routes![index])
+        .launch()
+        .await
+        .unwrap();
 }
